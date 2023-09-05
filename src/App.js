1c687e90668fb8b0fc61b744348bcb84354d6845
박혜민 <hyeminpak@gmail.com>
2023-09-06 02:41:44 +0900

    todoList 수정하는 기능생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import TodoItemCreate from './components/TodoItemCreate';
+import { useRecoilValue } from 'recoil';
+import { todoListState } from './todoAtom';
+import TodoItem from './components/TodoItem';
 
 function App() {
+  const todoList = useRecoilValue(todoListState);
+  console.log(todoList);
   return (
     <div className="App">
       <TodoItemCreate />
+      {todoList.map(todo => <TodoItem key={todo.id} item={todo} />)}
     </div>
   );
 }
